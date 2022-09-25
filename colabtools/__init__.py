af3e4b317f89ccbd48ac1035e0313d50e8ee6537
John Ward <john@johnward.io>
2022-09-24 22:40:19 -0400

    stringify gpu names and automatically configure drive defaults

--- colabtools/__init__.py
+++ colabtools/__init__.py
@@ -1,6 +1,10 @@
+from . import drive, tensorflow
+
 try:
     # This model supports running locally, or on Google Colab. If running on Google Colab, it will automatically detect the environment, install any missing dependencies, and activate the GPU or TPU if available.
-    from google.colab import drive
+    import google.colab
     COLAB = True
+    tensorflow.set_default_strategy()
+    drive.mount()
 except:
     COLAB = False
--- colabtools/tensorflow.py
+++ colabtools/tensorflow.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 
+def get_gpu_names():
+    return [gpu.name.strip('/physical_device:') for gpu in tf.config.list_physical_devices('GPU')]
+
 def get_strategy():
     try:
         tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
@@ -13,7 +16,8 @@ def get_strategy():
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
         print(f'Using GPU(s): {gpus}')
-        return tf.distribute.MirroredStrategy(gpus)
+        return tf.distribute.MirroredStrategy(devices=get_gpu_names())
+    print('Using CPU')
     return tf.distribute.MirroredStrategy()
 
 def set_default_strategy():
